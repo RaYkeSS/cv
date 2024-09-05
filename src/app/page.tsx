28b13e9a9cd3f8418d2effb2ca1861040a61bd18
RaYkeSS <raykess25rus@gmail.com>
2024-09-05 15:56:54 +0700

    starting project

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  const [value, setValue] = React.useState(0);
+  return (
+    <div className={styles.page}>
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Box>
+    </div>
+  );
+}
