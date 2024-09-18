3e96094957f72ba737ba0f48caea8a7093f46156
RaYkeSS <raykess25rus@gmail.com>
2024-09-18 19:06:22 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["raykess.ru", "localhost:3000"],
+    },
+  },
+};
 
 export default nextConfig;
