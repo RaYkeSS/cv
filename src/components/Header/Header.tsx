19596353a88770c92118d0180dee7ac92cc8ba21
RaYkeSS <raykess25rus@gmail.com>
2024-12-18 15:55:12 +0700

    media css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+'use client'
+import {useState} from "react";
 import Image from "next/image";
 import Button from "@mui/material/Button";
 import NextLink from "next/link";
@@ -21,36 +23,46 @@ interface HeaderProps {
 }
 
 export default function Header({ content }: HeaderProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Image alt="raykess logo" src={logo} width={36} height={36} />
         <nav>
-          <ul className={styles.nav__list}>
+          <div className={styles.burger} onClick={toggleMenu}>
+            <span className={`${styles.line} ${isOpen ? styles.open : styles.closed}`}></span>
+            <span className={`${styles.line} ${isOpen ? styles.open : styles.closed}`}></span>
+            <span className={`${styles.line} ${isOpen ? styles.open : styles.closed}`}></span>
+          </div>
+          <ul className={`${styles.nav__list} ${isOpen ? styles.open : styles.closed}`}>
             <li className={styles.nav__item}>
               <Button component={NextLink} color="inherit" href="#about">
                 {content.about}
               </Button>
-            </li>
-            <li className={styles.nav__item}>
-              <Button component={NextLink} color="inherit" href="#skills">
-                {content.skills}
-              </Button>
-            </li>
-            <li className={styles.nav__item}>
-              <Button component={NextLink} color="inherit" href="#projects">
-                {content.projects}
-              </Button>
-            </li>
-            <li className={styles.nav__item}>
-              <Button component={NextLink} color="inherit" href="#contact">
-                {content.contact}
-              </Button>
-            </li>
-          </ul>
+              </li>
+              <li className={styles.nav__item}>
+                <Button component={NextLink} color="inherit" href="#skills">
+                  {content.skills}
+                </Button>
+              </li>
+              <li className={styles.nav__item}>
+                <Button component={NextLink} color="inherit" href="#projects">
+                  {content.projects}
+                </Button>
+              </li>
+              <li className={styles.nav__item}>
+                <Button component={NextLink} color="inherit" href="#contact">
+                  {content.contact}
+                </Button>
+              </li>
+            </ul>
         </nav>
         <div className={styles.button_group}>
-          <LanguageSelector />
+        <LanguageSelector />
           <ThemeSwitch />
           <Button href={"/pdf/cv.pdf"} variant="contained">
             {content.download}
